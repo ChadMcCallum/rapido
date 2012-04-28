3362b94383754e4a70ebd0f1c4b457bb7c500cc9
Philip Brockmeyer <PhilipBrockmeyer@gmail.com>
2012-04-28 09:55:28 -0600

    added the data, created a data parser

--- Rapido/MainPage.xaml.cs
+++ Rapido/MainPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.IO;
 
 namespace Rapido
 {
@@ -19,6 +20,9 @@ namespace Rapido
         public MainPage()
         {
             InitializeComponent();
+
+
+            //PathParser.Parse(File.OpenText("MapData.xml").ReadToEnd());
         }
     }
 }
---
+++ Rapido/PathParser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace Rapido
+{
+    public class PathParser
+    {
+        public static IEnumerable<Path> Parse(String pathKml)
+        {
+            var xDoc = XDocument.Parse(pathKml);
+
+            foreach (var placemarkElem in xDoc.Descendants("Placemark"))
+            {
+                var coordinatesElem = placemarkElem.Descendants("coordinates").Single();
+                var descriptionElem = placemarkElem.Descendants("description").Single();
+
+                var coordinatePairs = coordinatesElem.Value.Split(' ');
+
+                var coordinateList = new List<Path.Coordinate>();
+
+                foreach (var coordiate in coordinatePairs)
+                {
+                    coordinateList.Add (new Path.Coordinate { Latitude = Double.Parse(coordiate.Split(',')[0]), Longitude = Double.Parse(coordiate.Split(',')[1]) } );
+                }
+
+                var path = new Path
+                {
+                     Description = descriptionElem.Value,
+                     Coordinates = coordinateList
+                };
+
+                yield return path;
+            }
+        }
+    }
+}
